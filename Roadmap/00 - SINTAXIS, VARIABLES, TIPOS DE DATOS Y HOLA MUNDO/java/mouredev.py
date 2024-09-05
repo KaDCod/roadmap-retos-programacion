8c036c9f6d2a6a3aadbca327b1f0a1d8a435e0bd
KaDCod <diazsegurakarol@gmail.com>
2024-09-05 16:13:19 -0500

    mouredev.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/mouredev.py
@@ -0,0 +1,10 @@
+#https://java.com/es/
+
+#Comentario en linea
+/*Esto tambien es un 
+comentario en varias lineas*/
+
+String myVariable = "Mi variable";
+Sreing myVariable = "Nuevo valor d emi variable";
+
+String myConstante = "Mi constante";
